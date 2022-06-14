9128a263b1e5b70cc90c01cb424b03b44fe3102a
egeakman <egeakmanegeakman@hotmail.com>
2022-06-15 02:16:58 +0300

    Fix

--- setup.py
+++ setup.py
@@ -26,12 +26,10 @@ setup(
     description="Gcode to Zaxe Converter | executable: g2z",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="2022.6.15",
+    version="2022.6.15-1",
     license="AGPLv3",
-    download_url="https://github.com/egeakman/gcode2zaxe/archive/2022.6.15.tar.gz",
-    packages=find_packages(
-        where="gcode2zaxe", exclude=["tests", "resources", "gcode2zaxegui"]
-    ),
+    download_url="https://github.com/egeakman/gcode2zaxe/archive/2022.6.15-1.tar.gz",
+    packages=find_packages(where=".", exclude=["tests", "resources", "gcode2zaxegui"]),
     python_requires=">=3.8",
     entry_points={
         "console_scripts": [
